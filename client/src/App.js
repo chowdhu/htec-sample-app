68ca79b87d1915cac570830520d05c8c728ea5e8
chowdhu <chowdh@gmail.com>
2022-12-13 12:56:53 -0600

    Done

--- client/src/App.js
+++ client/src/App.js
@@ -50,7 +50,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="products-app">
-        <h1>Humanitec Sample App - Veterans United DEMO</h1>
+        <h1>Humanitec Sample App - Veterans United </h1>
         <h2>Task list</h2>
         <ProductCreator onCreate={this.handleProductCreated} />
         <Products products={this.state.products} loading={this.state.productsLoading} />
